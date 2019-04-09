083fdb213e8545f10408b1a9a6a010bac75d6790
BioEBoi <38054899+BioEBoi@users.noreply.github.com>
2019-04-08 17:49:36 -0700

    Add files via upload

--- specConcentrationCalc.m
+++ specConcentrationCalc.m
@@ -4,9 +4,12 @@
 arg_list = argv ();
 
 sampleOne = cell2mat(arg_list(1:length(arg_list)/3));
+sampleOne
 sampleTwo = cell2mat(arg_list((length(arg_list)/3):(2*length(arg_list))/3));
+sampleTwo
 BaseLine = cell2mat(arg_list((2*length(arg_list)/3):end));
+BaseLine
 SumOfSamples = [sampleOne;sampleTwo];
 
- relativeConcentration = BaseLine\(SumOfSamples)
+ relativeConcentration = BaseLine/(SumOfSamples)
 
