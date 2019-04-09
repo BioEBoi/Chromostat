cea063be970392c9c606279448683d3a6ece90e3
BioEBoi <38054899+BioEBoi@users.noreply.github.com>
2019-04-09 11:57:40 -0700

    Add files via upload

--- specConcentrationCalc.m
+++ specConcentrationCalc.m
@@ -4,21 +4,21 @@ clear all, clc, close all;
 
 
 arg_list = argv();
-arg_list_matrix = str2num(cell2mat(arg_list)).';
+arg_list_matrix = str2num(cell2mat(arg_list)).'
 ##typeinfo(arg_list_matrix)
 ##InputSize = size(arg_list_matrix)
 
-sampleOne = (arg_list_matrix(1:length(arg_list)/3));
+sampleOne = (arg_list_matrix(1:length(arg_list)/3))
 ##SampleOneSize = size(sampleOne)
-sampleTwo = (arg_list_matrix((length(arg_list_matrix)/3) + 1:(2*length(arg_list_matrix))/3));
-##SampleTwoSize = size(sampleTwo)
-##sampleTwo
-SumOfSamples = [sampleOne; sampleTwo];
-##SizeOfSum = size(SumOfSamples)
-BaseLine = (arg_list_matrix((2*length(arg_list_matrix)/3) + 1:end));
-##BaseLineSize = size(BaseLine)
+sampleTwo = (arg_list_matrix((length(arg_list_matrix)/3) + 1:(2*length(arg_list_matrix))/3))
+SampleTwoSize = size(sampleTwo)
+sampleTwo
+SumOfSamples = [sampleOne; sampleTwo]
+SizeOfSum = size(SumOfSamples)
+BaseLine = (arg_list_matrix((2*length(arg_list_matrix)/3) + 1:end))
+BaseLineSize = size(BaseLine)
 
-##BaseLine
+
 
 ##SumOfSamples
 ##size(SumOfSamples)
