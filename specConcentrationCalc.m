a99af1171c8a51aefa25cac3351155ed99672036
BioEBoi <38054899+BioEBoi@users.noreply.github.com>
2019-04-09 10:51:07 -0700

    Add files via upload

--- specConcentrationCalc.m
+++ specConcentrationCalc.m
@@ -1,20 +1,24 @@
 #! /bin/octave -qf
 
+clear all, clc, close all;
 
-arg_list = argv ();
+arg_list = arg();
+arg_list_matrix = cell2mat(arg_list)
 
-sampleOne = cell2mat(arg_list(1:length(arg_list)/3)).';
-length(arg_list)
-sampleOne
-sampleTwo = cell2mat(arg_list((length(arg_list)/3) + 1:(2*length(arg_list))/3)).';
-sampleTwo
-BaseLine = cell2mat(arg_list((2*length(arg_list)/3) + 1:end)).';
-BaseLine
-SumOfSamples = [sampleOne(1) , sampleOne(2); sampleTwo(1), sampleTwo(2)]
-SumOfSamples
-size(SumOfSamples)
-size(BaseLine)
-BaseLine
+sampleOne = (arg_list_matrix(1:length(arg_list)/3))
+##length(arg_list)
+##sampleOne
+sampleTwo = cell2mat(arg_list((length(arg_list_matrix)/3) + 1:(2*length(arg_list_matrix))/3))
+##sampleTwo
+SumOfSamples = [sampleOne; sampleTwo]
+BaseLine = (arg_list_matrix((2*length(arg_list_matrix)/3) + 1:end))
 
- relativeConcentration = BaseLine/(SumOfSamples)
+##BaseLine
+
+##SumOfSamples
+##size(SumOfSamples)
+##size(BaseLine)
+
+  
+ relativeConcentration = BaseLine * inverse(SumOfSamples)
 
