a1d11cd2d8c3b3a95b76cac3554313e3c70899e7
BioEBoi <38054899+BioEBoi@users.noreply.github.com>
2019-04-09 10:59:52 -0700

    Add files via upload

--- specConcentrationCalc.m
+++ specConcentrationCalc.m
@@ -5,6 +5,7 @@ clear all, clc, close all;
 
 arg_list = argv();
 arg_list_matrix = cell2mat(arg_list).'
+typeinfo(arg_list_matrix)
 InputSize = size(arg_list_matrix)
 
 sampleOne = (arg_list_matrix(1:length(arg_list)/3))
