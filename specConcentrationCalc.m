e4b564fb117363be2b7b16085f6782d764e03bfc
BioEBoi <38054899+BioEBoi@users.noreply.github.com>
2019-04-08 17:44:09 -0700

    Add files via upload

--- specConcentrationCalc.m
+++ specConcentrationCalc.m
@@ -4,7 +4,7 @@
 arg_list = argv ();
 
 sampleOne = arg_list(1:length(arg_list)/3);
-sampleTwo = arg_lis((length(arg_list)/3):(2*length(arg_list))/3);
+sampleTwo = arg_list((length(arg_list)/3):(2*length(arg_list))/3);
 BaseLine = arg_list((2*length(arg_list)/3):end);
 SumOfSamples = [sampleOne;sampleTwo];
 
