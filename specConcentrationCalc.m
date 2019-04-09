8efcd4f3b2ce2ed2f61616c7830b1e3d610ba0a4
BioEBoi <38054899+BioEBoi@users.noreply.github.com>
2019-04-09 11:23:12 -0700

    Add files via upload

--- specConcentrationCalc.m
+++ specConcentrationCalc.m
@@ -4,21 +4,19 @@ clear all, clc, close all;
 
 
 arg_list = argv();
-arg_list_matrix = str2num(cell2mat(arg_list)).'
-typeinfo(arg_list_matrix)
-InputSize = size(arg_list_matrix)
+arg_list_matrix = str2num(cell2mat(arg_list)).';
+##typeinfo(arg_list_matrix)
+##InputSize = size(arg_list_matrix)
 
-sampleOne = (arg_list_matrix(1:length(arg_list)/3))
-SampleOneSize = size(sampleOne)
-##length(arg_list)
-##sampleOne
-sampleTwo = (arg_list_matrix((length(arg_list_matrix)/3) + 1:(2*length(arg_list_matrix))/3))
-SampleTwoSize = size(sampleTwo)
+sampleOne = (arg_list_matrix(1:length(arg_list)/3));
+##SampleOneSize = size(sampleOne)
+sampleTwo = (arg_list_matrix((length(arg_list_matrix)/3) + 1:(2*length(arg_list_matrix))/3));
+##SampleTwoSize = size(sampleTwo)
 ##sampleTwo
-SumOfSamples = [sampleOne; sampleTwo]
-SizeOfSum = size(SumOfSamples)
-BaseLine = (arg_list_matrix((2*length(arg_list_matrix)/3) + 1:end))
-BaseLineSize = size(BaseLine)
+SumOfSamples = [sampleOne; sampleTwo];
+##SizeOfSum = size(SumOfSamples)
+BaseLine = (arg_list_matrix((2*length(arg_list_matrix)/3) + 1:end));
+##BaseLineSize = size(BaseLine)
 
 ##BaseLine
 
