9644e0f446e7c60882fd1186042399cf0f056e01
BioEBoi <38054899+BioEBoi@users.noreply.github.com>
2019-04-08 17:42:11 -0700

    Add files via upload

---
+++ specConcentrationCalc.m
@@ -0,0 +1,12 @@
+#! /bin/octave -qf
+
+
+arg_list = argv ();
+
+sampleOne = arg_list(1:length(arg_list)/3);
+sampleTwo = arg_lis((length(arg_list)/3):(2*length(arg_list))/3);
+BaseLine = arg_list((2*length(arg_list)/3):end);
+SumOfSamples = [sampleOne;sampleTwo];
+
+ relativeConcentration = BaseLine/SumOfSamples
+
