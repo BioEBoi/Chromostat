03c74a2915f8a8864d25bd649a87ef477a85e800
BioEBoi <38054899+BioEBoi@users.noreply.github.com>
2019-04-08 17:50:25 -0700

    Add files via upload

--- specConcentrationCalc.m
+++ specConcentrationCalc.m
@@ -4,6 +4,7 @@
 arg_list = argv ();
 
 sampleOne = cell2mat(arg_list(1:length(arg_list)/3));
+length(arg_list)
 sampleOne
 sampleTwo = cell2mat(arg_list((length(arg_list)/3):(2*length(arg_list))/3));
 sampleTwo
