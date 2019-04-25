66ce446c1b6ab9992b91f136cd69cf8750e58e84
BioEBoi <38054899+BioEBoi@users.noreply.github.com>
2019-04-25 10:42:11 -0700

    Add files via upload

--- specConcentrationCalc.m
+++ specConcentrationCalc.m
@@ -8,24 +8,28 @@ arg_list_matrix = str2num(cell2mat(arg_list));
 ##typeinfo(arg_list_matrix)
 ##InputSize = size(arg_list_matrix)
 
+baseLine = (arg_list_matrix((2*length(arg_list_matrix)/3) + 1:end));
+##BaseLineSize = size(BaseLine)
+
 sampleOne = arg_list_matrix(1:(length(arg_list_matrix)/3));
 ##SampleOneSize = size(sampleOne)
 
 sampleTwo = (arg_list_matrix((length(arg_list_matrix)/3) + 1:(2*length(arg_list_matrix))/3));
 ##SampleTwoSize = size(sampleTwo)
 
-SumOfSamples = [sampleOne; sampleTwo];
+absorbanceOne = log10(baseLine./sampleOne).';
+
+absorbanceTwo = log10(baseLine./sampleTwo).';
+
+absorbanceMixed = log10(baseLine./mixedSample).';
+
+
+sampleData = [absorbanceOne, absorbanceTwo];
 ##SizeOfSum = size(SumOfSamples)
 
-BaseLine = (arg_list_matrix((2*length(arg_list_matrix)/3) + 1:end));
-##BaseLineSize = size(BaseLine)
+x = absorbanceMixed\sampleData;
 
-
-
-##SumOfSamples
-##size(SumOfSamples)
-##size(BaseLine)
-
-  
- relativeConcentration = BaseLine/(SumOfSamples)
+Conc1 = (x(1)/sum(x));
+Conc2 = (1 - Conc1) * 100
+Conc1 = Conc1 * 100
 
