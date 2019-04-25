48bddb6fc6b5442b62502a53685e581617d8da36
BioEBoi <38054899+BioEBoi@users.noreply.github.com>
2019-04-25 12:35:24 -0700

    Add files via upload

--- specConcentrationCalc.m
+++ specConcentrationCalc.m
@@ -9,19 +9,19 @@ arg_list_matrix = str2num(cell2mat(arg_list));
 ##InputSize = size(arg_list_matrix)
 
 
-baseLine = arg_list_matrix(1: end/4);
-BaseLineSize = size(baseLine);
+baseLine = arg_list_matrix(1: end/4)
+##BaseLineSize = size(baseLine);
 
-sampleOne = arg_list_matrix(end/4 + 1: 2 * end/4);
+sampleOne = arg_list_matrix(end/4 + 1: 2 * end/4)
 ##SampleTwoSize = size(sampleTwo)
 
-sampleTwo = arg_list_matrix(2 * end/4 + 1: 3 * end/4);
+sampleTwo = arg_list_matrix(2 * end/4 + 1: 3 * end/4)
 ##SampleOneSize = size(sampleOne)
 
-mixedSample = arg_list_matrix(3 * end/4 + 1: end);
+mixedSample = arg_list_matrix(3 * end/4 + 1: end)
 
 absorbanceOne = log10(baseLine./sampleOne).';
-size(absorbanceOne);
+##size(absorbanceOne);
 
 absorbanceTwo = log10(baseLine./sampleTwo).';
 
@@ -29,15 +29,9 @@ absorbanceMixed = log10(baseLine./mixedSample).';
 
 
 sampleData = [absorbanceOne , absorbanceTwo];
-SizeOfSum = size(sampleData);
-
-printf("working on concentrations \n")
+##SizeOfSum = size(sampleData);
 
 x = absorbanceMixed\sampleData
-
-Conc1 = x(1) / (x(1) + x(2));
-Conc2 = 1 - Conc1;
-
 
 ##Conc1 = (x(1)/sum(x));
 ##Conc2 = (1 - Conc1) * 100
