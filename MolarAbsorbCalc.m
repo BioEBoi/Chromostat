8df521d26a1ead1a029d0cf3099ea3a862a974c5
BioEBoi <38054899+BioEBoi@users.noreply.github.com>
2019-05-31 12:20:59 -0700

    Add files via upload

--- MolarAbsorbCalc.m
+++ MolarAbsorbCalc.m
@@ -1,26 +1,24 @@
 #! /bin/octave
 
 clear all, clc, close all;
-disp("octave is working")
 
-#{
 arg_list = argv();
-arg_list_matrix = str2num(cell2mat(arg_list));
+arg_list_matrix = str2num(cell2mat(arg_list))
 
-colSize = arg_list_matrix(1);
+colSize = arg_list_matrix(1)
 
-opticalDensity = arg_list_matrix(2,colSize + 1).';
+opticalDensity = arg_list_matrix(2,colSize + 1).'
 
-matrixStart = 2 + length(colSize);
+matrixStart = 2 + length(colSize)
 
-epValOne = arg_list_matrix(matrixStart,matrixStart + colSize).';
-epValTwo = arg_list_matrix(matrixStart + colSize + 1,end).';
+epValOne = arg_list_matrix(matrixStart,matrixStart + colSize).'
+epValTwo = arg_list_matrix(matrixStart + colSize + 1,end).'
 
 epValTotal = [epValOne,epValTwo];
 
 printf("this is working")
 concentrations = opticalDensity/epValTotal
-#}
+
 
 
 
