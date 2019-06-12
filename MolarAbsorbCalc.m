73a34061f645a83d76fd48e92dd5cc9475a47ca7
BioEBoi <38054899+BioEBoi@users.noreply.github.com>
2019-06-12 15:50:37 -0700

    Add files via upload

--- MolarAbsorbCalc.m
+++ MolarAbsorbCalc.m
@@ -6,16 +6,16 @@ arg_list = argv();
 arg_list_matrix = str2num(cell2mat(arg_list))
 #arg_list_matrix = [3 1 2 3 4 5 6 7 8 9]
 
-colSize = arg_list_matrix(1)
+colSize = arg_list_matrix(1);
 
-opticalDensity = arg_list_matrix(2:colSize + 1).'
+opticalDensity = arg_list_matrix(2:colSize + 1).';
 
-matrixStart = 2 + colSize
+matrixStart = 2 + colSize;
 
 epValOne = arg_list_matrix(matrixStart:matrixStart + colSize - 1).'
 epValTwo = arg_list_matrix(matrixStart + colSize:end).'
 
-epValTotal = [epValOne,epValTwo]
+epValTotal = [epValOne,epValTwo];
 
 concentrations = opticalDensity\epValTotal
 
